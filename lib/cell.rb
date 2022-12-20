983513cac8df46c1383e181c2e84c3cf0f3357ce
Adam Bailey <ab67319@gmail.com>
2022-12-19 19:42:41 -0700

    Create example '3. can check if empty' and corresponding method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,9 +1,13 @@
 class Cell
   attr_reader :coordinate,
               :ship
-              
+
   def initialize(cell)
     @coordinate = cell
     @ship = nil
   end
+
+  def empty?
+    @ship == nil
+  end 
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -5,6 +5,7 @@ describe Cell do
   context "Iteration 1" do
     before(:each) do
       @cell = Cell.new("B4")
+      @cruiser = Ship.new("Cruiser", 3)
     end
 
     it '1. exists' do
@@ -14,6 +15,10 @@ describe Cell do
     it '2. has readable attributes' do
       expect(@cell.coordinate).to eq("B4")
       expect(@cell.ship).to eq(nil)
+    end
+
+    it '3. can check if empty' do
+      expect(@cell.empty?).to eq(true)
     end
   end
 end
