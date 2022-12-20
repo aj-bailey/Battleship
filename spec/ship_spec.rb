3971e9dfc27a05ed7a278b2e233b18642f7b3d98
Adam Bailey <ab67319@gmail.com>
2022-12-19 18:26:28 -0700

    Create example '2. has readable attributes' and corresponding method

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -9,5 +9,11 @@ describe Ship do
     it '1. exists' do
       expect(@cruiser).to be_a(Ship)
     end
+
+    it '2. has readable attributes' do
+      expect(@cruiser.name).to eq("Cruiser")
+      expect(@cruiser.length).to eq(3)
+      expect(@cruiser.health).to eq(3)
+    end
   end
 end
